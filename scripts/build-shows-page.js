1e80244b1a2a03705cb4e2a3f2d12bf62ffbd1ff
hnslyswhy <huanyuw01@gmail.com>
2021-11-18 20:50:57 -0800

    refactor DOM manipulation code for shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -31,8 +31,105 @@ const showList = [
   },
 ];
 
+/***** create & display shows *******/
+//create & append new section
+const main = document.querySelector(".main");
+const section = document.createElement("section");
+section.classList.add("shows");
+main.append(section);
+// add section heading
+const title = document.createElement("h2");
+title.innerText = "Shows";
+title.classList.add("shows__title");
+section.append(title);
+// add shows container
+const showsContainer = document.createElement("div");
+showsContainer.classList.add("shows__container");
+section.append(showsContainer);
+
+// show header
+function createShowHeader() {
+  let headerList = ["date", "venue", "location", ""];
+  const headerRow = document.createElement("div");
+  headerRow.classList.add("shows__row", "shows__row--display1");
+  headerList.forEach((item) => {
+    const rowHead = document.createElement("p");
+    rowHead.innerText = item.toUpperCase();
+    rowHead.classList.add("shows__head");
+    headerRow.append(rowHead);
+  });
+  showsContainer.append(headerRow);
+}
+createShowHeader();
+
+// show card
+showList.forEach((show) => {
+  let showCard = addCardElement(show);
+  showsContainer.append(showCard);
+});
+
+function addCardElement(show) {
+  let showCard = document.createElement("article");
+  showCard.classList.add("shows__row");
+
+  const showInfoList = Object.keys(show);
+  showInfoList.forEach(function (item) {
+    const infoHead = document.createElement("p");
+    infoHead.classList.add("shows__head", "shows__head--display"); // shows__table-date --> shows__info-head
+    infoHead.innerText = item.toUpperCase();
+    console.log(show.item);
+    showCard.append(infoHead);
+
+    const infoData = document.createElement("p");
+    if (item === "date") {
+      infoData.classList.add("shows__date");
+    } else {
+      infoData.classList.add("shows__data");
+    }
+    infoData.innerText = show[item]; //its a variable, need use [] instead of .
+    showCard.append(infoData);
+  });
+
+  let cardButton = document.createElement("button");
+  cardButton.classList.add("shows__btn");
+  cardButton.innerText = "BUY TICKETS";
+  showCard.append(cardButton);
+
+  return showCard;
+}
+
+/***** row hover & active effect *******/
+const rowList = document.querySelectorAll(".shows__row");
+for (let i = 1; i < rowList.length; i++) {
+  //hover effect
+  rowList[i].addEventListener("mouseover", () => {
+    rowList[i].classList.add("shows__row--hover");
+  });
+  rowList[i].addEventListener("mouseleave", () => {
+    rowList[i].classList.remove("shows__row--hover");
+  });
+
+  //active effect
+  rowList[i].addEventListener("click", function () {
+    //check if any active row, if there is, remove its active effect
+    rowList.forEach((row) => {
+      row.classList.forEach((el) => {
+        if (el === "shows__row--active") {
+          row.classList.remove("shows__row--active");
+        }
+      });
+    });
+    // remvoe hover effect for the clicked row to avoid effect conflicts
+    rowList[i].classList.remove("shows__row--hover");
+    // add active effect
+    rowList[i].classList.add("shows__row--active");
+  });
+}
+
+/***** create & display shows *******/
+/*
 function displayShows(arr) {
-  // create tags, innerText, add class for container and table frame
+  // create tags, innerText, add class for container
   const main = document.querySelector(".main");
   const section = document.createElement("section");
   section.classList.add("shows");
@@ -43,20 +140,26 @@ function displayShows(arr) {
 
   const table = document.createElement("article");
   table.classList.add("shows__table");
+
   const tableRow = document.createElement("div");
   tableRow.classList.add("shows__table-row", "shows__table-row--display1");
+
   const dateHead = document.createElement("p");
   dateHead.innerText = "DATE";
   dateHead.classList.add("shows__table-head");
+
   const venueHead = document.createElement("p");
   venueHead.innerText = "VENUE";
   venueHead.classList.add("shows__table-head");
+
   const locationHead = document.createElement("p");
   locationHead.innerText = "LOCATION";
   locationHead.classList.add("shows__table-head");
+
   const btnHead = document.createElement("p");
   btnHead.innerText = "";
   btnHead.classList.add("shows__table-head");
+
   // append tags
   main.append(section);
   section.append(title, table);
@@ -64,7 +167,7 @@ function displayShows(arr) {
   tableRow.append(dateHead, venueHead, locationHead, btnHead);
 
   for (let i = 0; i < arr.length; i++) {
-    // create table row content
+    // create row content
     const row = document.createElement("div");
     row.classList.add("shows__table-row");
     const data1 = document.createElement("p");
@@ -100,23 +203,4 @@ function displayShows(arr) {
 }
 
 displayShows(showList);
-
-/***** row hover & active effect *******/
-const rowList = document.querySelectorAll(".shows__table-row");
-for (let i = 1; i < rowList.length; i++) {
-  //hover effect
-  rowList[i].addEventListener("mouseover", () => {
-    rowList[i].classList.add("shows__table-row--hover");
-  });
-  rowList[i].addEventListener("mouseleave", () => {
-    rowList[i].classList.remove("shows__table-row--hover");
-  });
-
-  //active effect
-  rowList[i].addEventListener("click", function () {
-    rowList[i].classList.add("shows__table-row--active");
-  });
-  // rowList[i].addEventListener("", function () {
-  //   rowList[i].classList.remove("shows__table-row--active");
-  // });
-}
+*/
